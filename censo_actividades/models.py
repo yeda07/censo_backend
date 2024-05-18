9971e76628ce3d06b2e9630a7a0199792e3840fb
jhonierp <jhonierpasos9@gmail.com>
2024-05-18 12:31:51 -0500

    configuracion de staticfiles

--- censo_actividades/models.py
+++ censo_actividades/models.py
@@ -14,8 +14,8 @@ class CensoActividad(models.Model):
         (PENDIENTE, 'Pendiente'),
     ]
     
-    censo = models.ForeignKey('censos.Censo', on_delete=models.CASCADE)
-    actividad = models.ForeignKey('actividades.Actividad', on_delete=models.CASCADE)
+    censo = models.ForeignKey('censos.Censo', on_delete=models.SET_NULL, null=True, blank=True)
+    actividad = models.ForeignKey('actividades.Actividad', on_delete=models.SET_NULL, null=True, blank=True)
     estado = models.CharField(max_length=1, choices=CHOICES_ESTADO, default=PENDIENTE)
 
     def save(self, *args, **kwargs):
