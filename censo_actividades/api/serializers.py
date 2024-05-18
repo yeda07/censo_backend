f921aab76934075cb39001183e447e4527238345
jhonierp <jhonierpasos9@gmail.com>
2024-05-18 13:20:53 -0500

    correcion censo_act con multa null

--- censo_actividades/api/serializers.py
+++ censo_actividades/api/serializers.py
@@ -3,34 +3,47 @@ from censo_actividades.models import CensoActividad
 from multas.api.serializers import MultaSerializer
 from multas.models import Multa
 
-
 class CensoActSerializer(serializers.ModelSerializer):
-    multa = MultaSerializer()
+    multas = MultaSerializer(many=True, read_only=True)
+    multa_monto = serializers.DecimalField(max_digits=10, decimal_places=2, write_only=True, required=False)
+    fecha_pago = serializers.DateField(write_only=True, required=False)  # Nuevo campo para capturar la fecha de pago
 
     class Meta:
         model = CensoActividad
-        fields = ['censo', 'actividad', 'estado', 'multa']
+        fields = ['id', 'censo', 'actividad', 'estado', 'multas', 'multa_monto', 'fecha_pago']
 
     def create(self, validated_data):
-        multa_data = validated_data.pop('multa', None)
+        multa_monto = validated_data.pop('multa_monto', None)
+        fecha_pago = validated_data.pop('fecha_pago', None)
+        
         censo_actividad = CensoActividad.objects.create(**validated_data)
-        if multa_data:
-            Multa.objects.create(censo_actividad=censo_actividad, **multa_data)
+
+        if validated_data.get('estado') == 'P':
+            multa = Multa.objects.create(
+                censo_actividad=censo_actividad, 
+                monto=multa_monto or 100.00,
+                pagada=bool(fecha_pago),
+                fecha_pago=fecha_pago
+            )
+
         return censo_actividad
 
     def update(self, instance, validated_data):
-        multa_data = validated_data.pop('multa', None)
-        instance.censo = validated_data.get('censo', instance.censo)
-        instance.actividad = validated_data.get('actividad', instance.actividad)
-        instance.estado = validated_data.get('estado', instance.estado)
+        estado = validated_data.get('estado', instance.estado)
+        multa_monto = validated_data.pop('multa_monto', None)
+        fecha_pago = validated_data.pop('fecha_pago', None)
+
+        if estado == 'P' and instance.estado != 'P':
+            Multa.objects.get_or_create(censo_actividad=instance, defaults={'monto': multa_monto or 100.00})
+        
+        if fecha_pago and estado == 'P':
+            multa = Multa.objects.filter(censo_actividad=instance, pagada=False).first()
+            if multa:
+                multa.pagada = True
+                multa.fecha_pago = fecha_pago
+                multa.save()
+        
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
         instance.save()
-
-        if multa_data:
-            if instance.multa:
-                instance.multa.monto = multa_data.get('monto', instance.multa.monto)
-                instance.multa.fecha_pago = multa_data.get('fecha_pago', instance.multa.fecha_pago)
-                instance.multa.save()
-            else:
-                Multa.objects.create(censo_actividad=instance, **multa_data)
-
         return instance
