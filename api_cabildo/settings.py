793eacee4c4cef7635a2321cde41a394bf29dfd2
jhonierp <jhonierpasos9@gmail.com>
2024-05-28 23:35:48 -0500

    add cors true

--- api_cabildo/settings.py
+++ api_cabildo/settings.py
@@ -145,3 +145,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
